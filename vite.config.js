bb2f4cf6d1f843f03b2c1a2cd3cf7e0d5beafd46
ovoowovo1 <144707905+ovoowovo1@users.noreply.github.com>
2026-05-04 19:29:40 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-        chunkSizeWarningLimit: 4500,
+        chunkSizeWarningLimit: 5000,
         rollupOptions: {
             output: {
                 manualChunks: function (id) {
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 4500,
+    chunkSizeWarningLimit: 5000,
     rollupOptions: {
       output: {
         manualChunks(id) {
